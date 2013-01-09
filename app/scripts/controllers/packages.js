4aee00decee1758d7937d479c0ea5f9a9f07e922
KillFill <pneumann@gmail.com>
2013-01-09 16:18:25 -0300

    Enable network deletion

--- app/scripts/controllers/packages.js
+++ app/scripts/controllers/packages.js
@@ -1,8 +1,22 @@
 'use strict';
 
-fifoApp.controller('PackagesCtrl', function($scope, wiggle, status) {
+fifoApp.controller('PackagesCtrl', function($scope, wiggle, status, modal) {
 
     $scope.packages = {}
+
+    $scope.delete = function(el) {
+
+        modal.confirm({
+            btnClass: 'btn-danger',
+            btnText: 'Delete',
+            header: 'Confirm Package Deletion',
+            body: '<p><font color="red">Warning!</font> you are about to delete the package <b>' + el.pack.name + "</b> Are you 100% sure you really want to do this?</p>"
+        }, function() {
+            status.update('Will delete' + el.pack.name, {info: true})
+            wiggle.packages.delete({id: el.pack.name})
+        })
+
+    }
 
     $scope.show = function() {
 
