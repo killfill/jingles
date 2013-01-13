b8d8debbf1a541d47e94f0ece704d60dcd42363f
KillFill <pneumann@gmail.com>
2013-01-13 15:48:55 -0300

    Do some validations on the requirements of the packages.

--- app/scripts/controllers/new_package.js
+++ app/scripts/controllers/new_package.js
@@ -2,8 +2,6 @@
 
 fifoApp.controller('NewPackageCtrl', function($scope, $location, wiggle) {
 
-    $scope.rules = [{}];
-
     $scope.add_rule = function() {
         $scope.rules.push({})
     }
@@ -12,12 +10,45 @@ fifoApp.controller('NewPackageCtrl', function($scope, $location, wiggle) {
         $scope.rules.splice(idx, 1);
     }
 
+    var validateRequirements = function() {
+        var valid = true;
+
+        $scope.rules.forEach(function(item) {
+
+            /* If none of the fields are present, just ignore it. */
+            var nonePresent = !item['attribute'] && !item['condition'] && !item['weight'] && !item['value']
+            if (nonePresent) {
+                item.error = false
+                return;
+            }
+
+            if (!item['attribute'] || !item['condition'] || !item['weight'] || !item['value']) {
+                valid = false;
+                item.error = true;
+            }
+            else
+                item.error = false
+
+        })
+        return valid;
+
+    }
+
     $scope.create_package = function() {
+
+        if (!validateRequirements()) {
+            alert('Some rules are not valid. Please fix them');
+            return;
+        }
+
+
         var pkg = new wiggle.packages({
             quota: parseInt($scope.quota),
             ram: parseInt($scope.ram),
             cpu_cap: parseInt($scope.cpu_cap),
-            requirements: $scope.rules
+            requirements: $scope.rules.filter(function(item) {
+                return item['attribute'] && item['condition'] && item['weight'] && item['value']
+            })
         });
 
         pkg.$create({id: $scope.name},
@@ -30,4 +61,16 @@ fifoApp.controller('NewPackageCtrl', function($scope, $location, wiggle) {
             }
         );
     }
+
+
+    $scope.init = function() {
+        $scope.rules = [{}];
+
+        $scope.availableOptions = {
+            attribute: [ 'resource.free-memory', 'resource.l1hits', 'resource.l1miss', 'resource.l1size',
+                          'resource.provisioned-memory', 'resource.total-memory']
+        }
+    }
+
+    $scope.init()
 })
