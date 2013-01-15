ad18fee98eef2414d28176bae9d17ac142cd0ee8
KillFill <pneumann@gmail.com>
2013-01-15 12:01:45 -0300

    Minor changes

--- app/scripts/services/vm.js
+++ app/scripts/services/vm.js
@@ -59,7 +59,7 @@ fifoApp.factory('vmService', function($rootScope, wiggle, status, modal) {
             if (!vm.config)
                 return vm;
 
-            vm._package = vm.package || 'manual'
+            vm._package = vm.package || 'custom'
             vm._name = vm.config.alias || vm.uuid.split('-')[0]
             vm._ips = (vm.config.networks || [])
                 .filter(function(i) {return vm.config.networks.length<2 || i.primary=='true'})
