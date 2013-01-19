d3a9e9685a0ec7452a469965c016afb0c18be3b1
Heinz N. Gies <heinz@licenser.net>
2013-01-19 08:08:54 +0100

    Users can now change their passwords.

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -3,10 +3,11 @@
 fifoApp.controller('UserCtrl', function($scope, $routeParams, $location, wiggle, vmService, modal, status) {
     var uuid = $routeParams.uuid;
     wiggle.users.get({id: uuid}, function(res) {
+        res.groups = res.groups || [];
         $scope.user = res; //vmService.updateCustomFields(res)
         $scope.permissions = [];
-        $scope.user._groups = {}
-        res.groups.map(function (gid){
+        $scope.user._groups = {};
+        $scope.user.groups.map(function (gid){
             if ($scope.groups[gid]) {
                 $scope.user._groups[gid] = $scope.groups[gid]
             } else {
@@ -24,6 +25,22 @@ fifoApp.controller('UserCtrl', function($scope, $routeParams, $location, wiggle,
     $scope.delete_permission = function(permission) {
         console.log(permission);
     };
+
+    $scope.passwd = function () {
+
+        if ($scope.pass1 == $scope.pass2) {
+            wiggle.users.put({id: $scope.user.uuid},
+                             {password: $scope.pass1},
+                             function() {
+                                 alert("Password for user " +
+                                       $scope.user.name +
+                                       " changed.");
+                             });
+
+        } else {
+            alert("Passwords don't match!");
+        }
+    }
 
     $scope.leave_group = function(group) {
         console.log("delete:", $scope.group);
