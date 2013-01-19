7bbae7e5437e4e2e9680bca9ead62707554c5b12
Heinz N. Gies <heinz@licenser.net>
2013-01-19 07:39:50 +0100

    Improved user management.

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -4,6 +4,15 @@ fifoApp.controller('UserCtrl', function($scope, $routeParams, $location, wiggle,
     var uuid = $routeParams.uuid;
     wiggle.users.get({id: uuid}, function(res) {
         $scope.user = res; //vmService.updateCustomFields(res)
+        $scope.permissions = [];
+        $scope.user._groups = {}
+        res.groups.map(function (gid){
+            if ($scope.groups[gid]) {
+                $scope.user._groups[gid] = $scope.groups[gid]
+            } else {
+                $scope.user._groups[gid] = {uuid: gid};
+            }
+        });
         $scope.permissions = res.permissions.map(function(p) {
             return {
                 text: p.join("->"),
@@ -20,21 +29,33 @@ fifoApp.controller('UserCtrl', function($scope, $routeParams, $location, wiggle,
         console.log("delete:", $scope.group);
         wiggle.users.delete({id: $scope.user.uuid,
                              controller: 'groups',
-                             controller_id: group});
+                             controller_id: group},
+                           function(){
+                               delete $scope.user._groups[group];
+                           });
     };
 
     $scope.group_join = function() {
         console.log("join:", $scope.group);
         wiggle.users.put({id: $scope.user.uuid,
                           controller: 'groups',
-                          controller_id: $scope.group});
+                          controller_id: $scope.group},
+                         function() {
+                             $scope.user._groups[$scope.group] = $scope.groups[$scope.group];
+                         }
+                        );
     };
 
     $scope.init = function() {
-        $scope.groups = []
+        $scope.groups = [];
         wiggle.groups.list(function(ids) {
             ids.forEach(function(gid) {
-                $scope.groups.push(gid);
+                wiggle.groups.get({id: gid}, function(g) {
+                    $scope.groups[gid] = g;
+                    if ($scope.user._groups[gid]) {
+                        $scope.user._groups[gid] = g;
+                    }
+                });
             });
         })
     };
--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -4,6 +4,19 @@ fifoApp.controller('UsersCtrl', function($scope, wiggle, status) {
 
     $scope.users = {}
 
+    var group_cache=(function(){
+        var groups = {};
+        return function(g, callback) {
+            if (groups[g]) {
+                callback(groups[g]);
+            } else {
+                wiggle.groups.get({id: g}, function(group) {
+                    groups[g] = group;
+                    callback(group);
+                })
+            }
+        }
+    })();
     $scope.show = function() {
 
         wiggle.users.list(function (ids) {
@@ -11,15 +24,19 @@ fifoApp.controller('UsersCtrl', function($scope, wiggle, status) {
             ids.length > 0 && status.update('Loading users', {total: ids.length})
 
             ids.forEach(function(id) {
-
                 $scope.users[id] = {name: id}
                 wiggle.users.get({id: id}, function(res) {
-                    $scope.users[id] = addFields(res)
-                    status.update('Loading users', {add: 1})
-                })
-
-            })
-        })
+                    $scope.users[id] = addFields(res);
+                    var gs = $scope.users[id].groups || [];
+                    gs.forEach(function(g, i) {
+                        group_cache(g, function(go) {
+                            $scope.users[id].groups[i] = go.name;
+                        });
+                    });
+                    status.update('Loading users', {add: 1});
+                });
+            });
+        });
     }
 
     $scope.show()
