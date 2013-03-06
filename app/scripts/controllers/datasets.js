a3682c0ce434d5c615139cc7afff592eb8aea0e6
Heinz N. Gies <heinz@licenser.net>
2013-03-07 00:57:00 +0100

    Added import from datsets.at

--- app/scripts/controllers/datasets.js
+++ app/scripts/controllers/datasets.js
@@ -1,38 +1,53 @@
 'use strict';
 
-fifoApp.controller('DatasetsCtrl', function($scope, wiggle, status) {
+fifoApp.controller('DatasetsCtrl', function($scope, wiggle, status, datasetsat) {
 
     $scope.datasets = {}
 
-    $scope.import = function() {
+    $scope.import = function(dataset) {
+        var url = $scope.url
+        if (dataset)
+            url = 'http://datasets.at/datasets/' + dataset.uuid;
+
         wiggle.datasets.import({},
-                               {url: $scope.url},
+                               {url: url},
                                function(r) {
                                    console.log(r);
                                });
     };
 
     $scope.show = function() {
-
         wiggle.datasets.list(function (ids) {
 
             ids.length > 0 && status.update('Loading datasets', {total: ids.length})
 
             ids.forEach(function(id) {
-
+                $scope.datasets[id] = {}
                 wiggle.datasets.get({id: id},
-                    function success(res) {
-                        if (res) $scope.datasets[id] = addFields(res)
-                        status.update('Loading datasets', {add: 1})
-                    },
-                    function error (res) {
-                        //Maybe we should not even show the dataset?
-                        $scope.datasets[id] = {dataset: id}
-                        status.update('Loading datasets', {add: 1})
-                    }
-                )
+                                    function success(res) {
+                                        if (res) $scope.datasets[id] = addFields(res)
+                                        status.update('Loading datasets', {add: 1})
+                                    },
+                                    function error (res) {
+                                        //Maybe we should not even show the dataset?
+                                        $scope.datasets[id] = {dataset: id}
+                                        status.update('Loading datasets', {add: 1})
+                                    }
+                                   )
 
             })
+            datasetsat.datasets.list(function (data) {
+                console.log(data[0]);
+                $scope.datasetsat = data.map(function(e) {
+                    if ($scope.datasets[e.uuid]) {
+                        e.imported = true;
+                    } else {
+                        e.imported = false;
+                    }
+                    return e;
+                });
+            });
+
         })
     }
 
---
+++ app/scripts/services/datasetsat.js
@@ -0,0 +1,51 @@
+'use strict';
+
+fifoApp.factory('datasetsat', function($resource, $http) {
+
+
+    var endpoint = "http://datasets.at/"
+
+
+    var services = {
+        datasets: $resource(endpoint + 'datasets/:id',
+                            {id: '@id'}),
+    }
+
+    /* Response with list of strings are not $resource friendly..
+       https://groups.google.com/forum/#!msg/angular/QjhN9-UeBVM/UjSgc5CNDqMJ */
+    endpoint = endpoint.replace("\\", '');
+    ['datasets'].forEach(function(resource) {
+        services[resource].list = function(cb, error) {
+
+            /* This is another ugyly hack thanks to shared state and
+             * mutable variables. There seems to be no way to not
+             * send a header for just one request or service or resource ...
+             */
+            var token = $http.defaults.headers.common['X-Snarl-Token'];
+            delete $http.defaults.headers.common['X-Snarl-Token'];
+            var res = $http.get(endpoint + resource)
+                .success(cb)
+                .error(function(data) {
+                    error && error(data)
+                });
+            $http.defaults.headers.common['X-Snarl-Token'] = token;
+
+            return res
+        }
+    });
+
+    /* Gets with cache! */
+    services.datasets.get = function(obj, success, error) {
+        var token = $http.defaults.headers.common['X-Snarl-Token'];
+        delete $http.defaults.headers.common['X-Snarl-Token'];
+        res = $http.get(endpoint + 'datasets/' + obj.id, {cache: true})
+            .success(success)
+            .error(function(data) {
+                error && error(data)
+            });
+        $http.defaults.headers.common['X-Snarl-Token'] = token;
+        return res;
+    }
+    return services
+
+});
