08a65cf1e67f819f244a8e12c42d01e5eb6785ab
Heinz N. Gies <heinz@licenser.net>
2013-04-16 10:53:03 +0200

    Improved error descriptions.

--- app/scripts/services/hooks.js
+++ app/scripts/services/hooks.js
@@ -8,18 +8,15 @@ angular.module('fifoHooks', [], function($provide, $httpProvider) {
         $rootScope.loading = true;
 
         return function(promise) {
-            return promise.then(
-                function success (res) {
-                    $rootScope.loading = false;
-                    $rootScope.$broadcast('response', 200, res)
-                    return res;
-                },
-                function error(res) {
-                    $rootScope.loading = false;
-                    $rootScope.$broadcast('response', res.status, res)
-                    return $q.reject(res)
-                }
-            )
+            return promise.then(function success (res) {
+                $rootScope.loading = false;
+                $rootScope.$broadcast('response', res.status, res)
+                return res;
+            },function error(res) {
+                $rootScope.loading = false;
+                $rootScope.$broadcast('response', res.status, res)
+                return $q.reject(res)
+            });
         }
     })
 
@@ -28,31 +25,38 @@ angular.module('fifoHooks', [], function($provide, $httpProvider) {
 
 /* Listen for the conection events, and do something with them. Its referenced from app.js. */
 fifoApp.factory('hookListener', function($rootScope, $cookies, wiggle, user, status) {
-
     $rootScope.$on('response', function(evt, statusCode, res) {
-
         if (statusCode < 300 || statusCode == 404) return;
 
-        if (statusCode == 403) {
-
+        switch (statusCode) {
+        case 400:
+            status.error('The request did contain invalid or incomplete data.');
+            break;
+        case 403:
             /* When user is loggin in, we could get a 403 (access denied..) */
             if (res.config.url.indexOf('sessions') > -1)
                 return;
-
             //If the session is valid, its something with the permition
-            return wiggle.sessions.get({id: $cookies["X-Snarl-Token"]},
-                function success(data) {
-                    console.log('Not allowed', statusCode, res.config)
-                    return status.info('Not allowed')
-                },
-                function error() {
-                    console.log('Invalid session. Logging out.')
-                    return user.logout();
-                })
-        }
-
-        status.error('There was an error. See the details in the js console')
-        console.error('Error:', statusCode, res.config)
-    })
-
-})
+            return wiggle.sessions.get({id: $cookies["X-Snarl-Token"]}, function success(data) {
+                console.log('Not allowed', statusCode, res.config)
+                return status.info('Not allowed')
+            }, function error() {
+                console.log('Invalid session. Logging out.')
+                return user.logout();
+            });
+            break;
+        case 409:
+            status.error('The request failed because of a data conflict.');
+            break;
+        case 505:
+            status.error('There was an internal server error processing the request.<br/>' +
+                         'See the details in the browsers javascript console.');
+            console.error('Error:', statusCode, res.config);
+            break
+        default:
+            status.error('There was an unexpected error. See the details in the js console');
+            console.error('Error:', statusCode, res.config);
+            break;
+        };
+    });
+});
