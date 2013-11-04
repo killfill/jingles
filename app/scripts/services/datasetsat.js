5c0efb7eacb0f1daf540aed2025c04921f22ef93
Phillip Neumann <pneumann@gmail.com>
2013-11-03 22:47:44 -0300

    Get rid of the defualt header hack

--- app/scripts/services/datasetsat.js
+++ app/scripts/services/datasetsat.js
@@ -15,18 +15,11 @@ angular.module('fifoApp')
     ['datasets'].forEach(function(resource) {
         services[resource].list = function(cb, error) {
 
-            /* This is another ugyly hack thanks to shared state and
-             * mutable variables. There seems to be no way to not
-             * send a header for just one request or service or resource ...
-             */
-            var token = $http.defaults.headers.common['x-snarl-token'];
-            delete $http.defaults.headers.common['x-snarl-token'];
             var res = $http.get(endpoint + resource)
                 .success(cb)
                 .error(function(data) {
                     error && error(data)
                 });
-            $http.defaults.headers.common['x-snarl-token'] = token;
 
             return res
         }
@@ -34,14 +27,11 @@ angular.module('fifoApp')
 
     /* Gets with cache! */
     services.datasets.get = function(obj, success, error) {
-        var token = $http.defaults.headers.common['x-snarl-token'];
-        delete $http.defaults.headers.common['x-snarl-token'];
         res = $http.get(endpoint + 'datasets/' + obj.id, {cache: true})
             .success(success)
             .error(function(data) {
                 error && error(data)
             });
-        $http.defaults.headers.common['x-snarl-token'] = token;
         return res;
     }
     return services
